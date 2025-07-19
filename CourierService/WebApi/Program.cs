5225554879fcb76aa20a0472c59ec5027e6171b6
Nazar Bezuhlyi <vlm.9a.nazar.bezuglyi@gmail.com>
2025-07-19 15:18:07 +0300

    Use new packages

--- CourierService/WebApi/Program.cs
+++ CourierService/WebApi/Program.cs
@@ -1,104 +1,18 @@
 using Application;
 using Application.Abstractions.Data;
-using Asp.Versioning;
-using HealthChecks.UI.Client;
 using Infrastructure;
-using Infrastructure.Logging;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Npgsql;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
-using Scalar.AspNetCore;
-using Serilog;
-using WebApi.Infrastructure.ExceptionHandlers;
-using WebApi.Infrastructure.RouteTransformers;
+using OrderTrackingSystem.AspNet.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var environment = builder.Environment;
 
-builder.Host.UseSerilog((context, configuration) => 
-    configuration
-        .ReadFrom.Configuration(context.Configuration)
-        .Enrich.FromLogContext()
-        .Enrich.With<ActivityEnricher>());
+builder.Host.AddSerilog();
 
-builder.Configuration
-    .AddJsonFile("Configurations/appsettings.json")
-    .AddJsonFile($"Configurations/appsettings.{environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
+builder.Configuration.AddConfiguration(environment);
 
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService("CourierService"))
-    .WithMetrics(metrics => metrics
-        .AddAspNetCoreInstrumentation()
-        .AddMeter("Microsoft.AspNetCore.Hosting")
-        .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
-        .AddMeter("System.Net.Http")
-        .AddMeter("System.Net.NameResolution")
-        .AddRuntimeInstrumentation()
-        .AddProcessInstrumentation()
-        .AddPrometheusExporter())
-    .WithTracing(tracing =>
-    {
-        tracing
-            .AddAspNetCoreInstrumentation()
-            .AddEntityFrameworkCoreInstrumentation()
-            .AddNpgsql()
-            .AddOtlpExporter();
-    });;
-
-builder.Services.AddOpenApi(options =>
-{
-    options.AddDocumentTransformer((document, _, _) =>
-    {  
-        document.Info = new OpenApiInfo
-        {
-            Title = "Courier Service API",
-            Version = "v1",
-        };
-        
-        return Task.CompletedTask;
-    });
-});
-
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        context.ProblemDetails.Instance =
-            $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
-
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-
-        var activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
-        context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
-    };
-});
-
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddControllers(opts =>
-    opts.Conventions.Add(new RouteTokenTransformerConvention(new ToKebabParameterTransformer())));
-
-builder.Services.AddApiVersioning(options =>
-    {
-        options.DefaultApiVersion = new ApiVersion(1);
-        options.ReportApiVersions = true;
-        options.AssumeDefaultVersionWhenUnspecified = true;
-        options.ApiVersionReader = new UrlSegmentApiVersionReader();
-    })
-    .AddMvc()
-    .AddApiExplorer(options =>
-    {
-        options.GroupNameFormat = "'v'V";
-        options.SubstituteApiVersionInUrl = true;
-    });
+builder.Services.AddWeb(builder.Configuration);
 
 builder.Services.AddApplication();
 
@@ -106,36 +20,7 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseExceptionHandler();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference(options =>
-    {
-        options.Servers = Array.Empty<ScalarServer>();
-    });
-}
-
-app.MapPrometheusScrapingEndpoint();
-
-app.UseSerilogRequestLogging(options =>
-{
-    options.IncludeQueryInRequestPath = true;
-    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
-    {
-        diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
-    };
-});
-
-app.UseStatusCodePages();
-
-app.UseHttpsRedirection();
-
-app.MapHealthChecks("/_health", new HealthCheckOptions
-{
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
+app.UseWeb();
 
 app.MapControllers();
 

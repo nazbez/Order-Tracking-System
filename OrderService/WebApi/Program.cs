99ca67f878ef9aeb8ff6d63114381a0f706ea039
Nazar Bezuhlyi <vlm.9a.nazar.bezuglyi@gmail.com>
2025-08-25 21:23:34 +0300

    Add Tracking service & grpc

--- OrderService/WebApi/Program.cs
+++ OrderService/WebApi/Program.cs
@@ -3,6 +3,8 @@ using Application.Abstractions.Data;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using OrderTrackingSystem.AspNet.Extensions;
+using WebApi.GrpcServices;
+using WebApi.GrpcServices.Interceptors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +17,15 @@ builder.Configuration.AddConfiguration(environment);
 builder.Services.AddWeb(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddGrpc(options => 
+    options.Interceptors.Add<LoggingInterceptor>());
 
 var app = builder.Build();
 
 app.UseWeb();
 
 app.MapControllers();
+app.MapGrpcService<OrderService>();
 
 if (app.Environment.IsDevelopment())
 {
